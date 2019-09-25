a3dca7234bfeff1b01c94d3f7a19f4c849d3e5ae
micaelgoms <micaellgoms@gmail.com>
2019-09-24 22:28:49 -0300

    feat(kolmogorov): implementation to komogorov

--- 1_atividade/kolmogorov.py
+++ 1_atividade/kolmogorov.py
@@ -2,7 +2,62 @@
 ## Trabalho de Processos Estocásticos
 ## Método de Kolmogorov
 
-from simple import main 
+import numpy as np
+
+class Matriz:
+    def __init__(self, steps = 1): 
+        self.mat = np.loadtxt('matriz')
+        self.vpi = np.loadtxt('vpi')
+        self.steps = steps
+
+    def getMatrix(self):
+        return self.mat
+
+    def printMat(self):
+        print(self.mat)
+
+    def printVpi(self):
+        print(self.vpi)
+
+    def isStochastic(self):
+        for i in range(len(self.mat)):
+            cont = 0
+            
+            for j in range(len(self.mat[i])):
+                cont += self.mat[i][j]
+                if cont > 1:
+                    return False
+
+        return True
+
+    def Kolmogorov(self, start, end, steps):
+        states = len(self.mat)
+
+        if steps == 1:
+            return self.mat[start][end]
+
+        value = 0
+        for k in range(states):
+            value += self.mat[start][k] * self.Kolmogorov(k, end, steps-1)
+
+        return value
+
+# Main
+def main():
+    steps = int(input("insira a quantidade de Passos:   "))
+    start = int(input("insira o estado inicial:   "))
+    end = int(input("insira o estado final:   "))
+
+    mat = Matriz(steps)
+
+    if mat.isStochastic():
+        resul = mat.Kolmogorov(start, end, steps)
+        print("saída:")
+        print(resul)
+
+    else:
+        print("Não é Estocástico")
+
 
 # Local Testes
 if __name__ == '__main__':
--- 1_atividade/simple.py
+++ 1_atividade/simple.py
@@ -3,6 +3,7 @@
 ## Método simples ulizando matriz de transição
 
 import numpy as np
+import sys
 
 class Matriz:
     def __init__(self, steps = 1): 
@@ -19,18 +20,45 @@ class Matriz:
     # Multiplicação de Matrizes
     def multMratix(self):
         resul = self.mat
-        for _ in range(self.steps):
+        for _ in range(self.steps - 1):
             resul = np.dot(self.mat, resul)
 
         return resul
 
+    def isStochastic(self):
+        for i in range(len(self.mat)):
+            cont = 0
+
+            for j in range(len(self.mat[i])):
+                cont += self.mat[i][j]
+                if cont > 1:
+                    return False
+
+        return True
+
 # Main
 def main():
-    mat = Matriz(int(input()))
+    steps = int(input("insira a quantidade de Passos:   "))
+    mat = Matriz(steps)
 
-    print(mat.multMratix())
-    print('VPI')
-    print(np.dot(mat.multMratix(), mat.vpi))
+    if mat.isStochastic():
+        if len(sys.argv) >= 2:
+            if sys.argv[1] == "vpi":
+                
+                # Pra testar com VPI tem passar 'vpi' como argumento na compilação
+                
+                print("\nVPI:")
+                print(np.dot(mat.multMratix(), mat.vpi))
+
+            else:
+                print("Argumento não definido!")
+
+        else:
+            print("Matriz de transição:")
+            print(mat.multMratix())
+
+    else:
+        print("Não é Estocástico")
 
 # Local Testes
 if __name__ == '__main__':
